42b7cc4769846c1835b9bdf2cfc57ad5087d732b
DevaAKhan <55737290+DevaAKhan@users.noreply.github.com>
2020-06-18 18:09:15 +0700

    mytoken.py

---
+++ mytoken.py
@@ -0,0 +1,13 @@
+TOKEN= ' 1179009599:AAEHGtgMxNb8jyJZbeftUB3esBLLOr3i46s'
+BAHASA = '🎈🎈 Selamat Datang 🎈🎈\n'\
+         'Pilih Bahasa (Choose language)\n'\
+        '👉 ketik /ind - untuk bahasa Indonesia 🇮🇩\n'\
+        '👉 ketik /eng - untuk bahasa Inggris 🇺🇸\n'
+SAPAE = 'Welcome ‼\n' \
+        'My name is Khan I was made and designed by Deva Abel Khan 👨🏼\n'\
+        '>>> Try <<<\n' \
+        '👉 type /pp - to see my profile picture 🖼\n' \
+        '>>> Task database connected bots <<<\n' \
+        '👉 type /datasiswa - to see student data in the database 💾\n'\
+        '>>> Help <<<\n'\
+        '👉 type /help - to know all my command 🆘\n'
